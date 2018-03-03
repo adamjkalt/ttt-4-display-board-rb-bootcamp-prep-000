000953708e3a3c3f4085d8eb84a24d7d893b8cb9
Adam Kalt <adam.kalt.1@gmail.com>
2018-03-03 22:04:56 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,6 @@
 # out the current state.
 def display_board(board)
   board = [" "," "," "," "," "," "," "," "," "]
-  display_board(board)
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
